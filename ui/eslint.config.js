81f7dac48051e8e04f2236761d6ad0fbdb068536
Florian Kinder <florian.kinder@enthus.de>
2026-02-28 17:44:15 +0100

    chore: configure ESLint with browser globals and TS-aware unused vars
    Add globals package for browser environment, use @typescript-eslint/no-unused-vars
    with _ prefix pattern, and add node globals for test files.

--- ui/eslint.config.js
+++ ui/eslint.config.js
@@ -1,16 +1,46 @@
 import js from "@eslint/js";
 import solid from "eslint-plugin-solid/configs/typescript";
 import * as tsParser from "@typescript-eslint/parser";
+import tsPlugin from "@typescript-eslint/eslint-plugin";
+import globals from "globals";
 
 export default [
   js.configs.recommended,
   {
     files: ["**/*.{ts,tsx}"],
     ...solid,
+    plugins: {
+      ...solid.plugins,
+      "@typescript-eslint": tsPlugin,
+    },
     languageOptions: {
       parser: tsParser,
       parserOptions: {
         project: "tsconfig.json",
+      },
+      globals: {
+        ...globals.browser,
+      },
+    },
+    rules: {
+      ...solid.rules,
+      // Use TS-aware version that understands _ prefix and type imports
+      "no-unused-vars": "off",
+      "@typescript-eslint/no-unused-vars": [
+        "warn",
+        {
+          argsIgnorePattern: "^_",
+          varsIgnorePattern: "^_",
+          ignoreRestSiblings: true,
+        },
+      ],
+    },
+  },
+  {
+    files: ["**/__tests__/**/*.{ts,tsx}", "**/*.test.{ts,tsx}"],
+    languageOptions: {
+      globals: {
+        ...globals.node,
       },
     },
   },
