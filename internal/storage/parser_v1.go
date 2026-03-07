efba845033af0ccad5e09caa316f719f4864349f
Florian Kinder <florian.kinder@enthus.de>
2026-03-07 15:21:03 +0100

    fix: update sector event format comment to include color field

--- internal/storage/parser_v1.go
+++ internal/storage/parser_v1.go
@@ -179,7 +179,7 @@ func parseEventArray(evtArr []interface{}) *Event {
 		return event
 	}
 
-	// Captured/contested sector events: [frameNum, "type", [objectType, unitName, side?, [posX, posY, posZ]?]]
+	// Captured/contested sector events: [frameNum, "type", [objectType, unitName, side?, color?, [posX, posY, posZ]?]]
 	if event.Type == "captured" || event.Type == "contested" {
 		if len(evtArr) > 2 {
 			if arr, ok := evtArr[2].([]interface{}); ok {
